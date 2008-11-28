64344790b165b283b1f7c49df31f1691fff85705
cmsbuild <>
2008-11-28 14:55:49 +0000

    This commit was manufactured by cvs2git to create tag 'Calibration- EcalCalibAlgos-phisym'.
    Sprout from master 2008-11-28 14:55:48 UTC Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch> 'temporary version to allow phisym stand alone'
    Cherrypick from master 2008-06-09 09:27:58 UTC Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch> 'Changes for alignable calo geometry, a bug fix':
        interface/PhiSymmetryCalibration.h
        src/PhiSymmetryCalibration.cc
    Delete:
        data/electronRecalibSCAssociator.cfi
        data/zeeCalibration.cff
        interface/BlockSolver.h
        interface/ClusterFillMap.h
        interface/EcalEleCalibLooper.h
        interface/ElectronCalibration.h
        interface/ElectronCalibrationUniv.h
        interface/ElectronRecalibSuperClusterAssociator.h
        interface/IMACalibBlock.h
        interface/InvRingCalib.h
        interface/L3CalibBlock.h
        interface/MatrixFillMap.h
        interface/Pi0FixedMassWindowCalibration.h
        interface/VEcalCalibBlock.h
        interface/VFillMap.h
        interface/ZeeCalibration.h
        interface/ZeeKinematicTools.h
        interface/ZeePlots.h
        interface/ZeeRescaleFactorPlots.h
        interface/miscalibExample.h
        python/electronRecalibSCAssociator_cfi.py
        python/zeeCalibration_cff.py
        scripts/MR_graphs.C
        scripts/csa07PhiSymCalibration.cfg.tmpl
        scripts/csa07PhiSymCalibration_Output.cfg
        scripts/csa07_stager.py
        scripts/csa07_submit.py
        scripts/listGSDR.txt
        scripts/phiSymCalibration_frontier_1.cfg.tmpl
        scripts/recHitsToAlCaPhiSymRecHits.cfg.tmpl
        src/BlockSolver.cc
        src/ClusterFillMap.cc
        src/EcalEleCalibLooper.cc
        src/ElectronCalibration.cc
        src/ElectronCalibrationUniv.cc
        src/ElectronRecalibSuperClusterAssociator.cc
        src/IMACalibBlock.cc
        src/InvRingCalib.cc
        src/L3CalibBlock.cc
        src/MatrixFillMap.cc
        src/Pi0FixedMassWindowCalibration.cc
        src/VFillMap.cc
        src/ZeeCalibration.cc
        src/ZeeKinematicTools.cc
        src/ZeePlots.cc
        src/ZeeRescaleFactorPlots.cc
        src/miscalibExample.cc
        test/SingleEle_IMAtest.cfg
        test/SingleEle_L3test.cfg
        test/SingleEle_cfg.py
        test/Single_PhiSim_cfg.py
        test/eleCalibration.cfg
        test/eleCalibrationUniv.cfg
        test/make_miscalibration.py
        test/miscalExample1.cfg
        test/miscalExample1_cfg.py
        test/miscalExample2.cfg
        test/miscalExample2_cfg.py
        test/miscalib_barrel_0.05.xml
        test/miscalib_endcap_0.05.xml
        test/nomiscalExample.cfg
        test/nomiscalExample_cfg.py
        test/non_existing_cell_endcap
        test/phiSymCalibration.cfg
        test/phiSymCalibration_frontier_1.cfg
        test/phiSymCalibration_frontier_output.cfg
        test/pi0FixedMassWindowCalibration.cfg
        test/zeeCalibration.cfg
        test/zeeCalibration_test.cfg

--- interface/PhiSymmetryCalibration.h
+++ interface/PhiSymmetryCalibration.h
@@ -24,7 +24,6 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 
-#include "CalibCalorimetry/CaloMiscalibTools/interface/CaloMiscalibMapEcal.h"
 #include "CondFormats/EcalObjects/interface/EcalIntercalibConstants.h"
 #include "DataFormats/DetId/interface/DetId.h"
 
@@ -61,7 +60,6 @@ class PhiSymmetryCalibration :  public edm::EDAnalyzer
 
   void getKfactors();
   void fillHistos();
-  void fillConstantsHistos();
 
   // private data members
 
@@ -75,27 +73,18 @@ class PhiSymmetryCalibration :  public edm::EDAnalyzer
   static const int  kEndcWedgesX = 100;
   static const int  kEndcWedgesY = 100;
 
-  static const int  kEndcEtaRings  = 39;
-
-  static const int  kNMiscalBinsEB = 21;
-  static const float  kMiscalRangeEB;
-
-  static const int  kNMiscalBinsEE = 81;
-  static const float  kMiscalRangeEE;
+  static const int  kEndcEtaRings = 39;
+  static const int  kNMiscalBins  = 21;
 
   // Transverse energy sum arrays
   double etsum_barl_[kBarlRings]  [kBarlWedges] [kSides];
   double etsum_endc_[kEndcWedgesX][kEndcWedgesX][kSides];
-  double etsum_endc_uncorr[kEndcWedgesX][kEndcWedgesX][kSides];
 
   double etsumMean_barl_[kBarlRings];
   double etsumMean_endc_[kEndcEtaRings];
 
-  unsigned int nhits_barl_[kBarlRings][kBarlWedges] [kSides];
-  unsigned int nhits_endc_[kEndcWedgesX][kEndcWedgesX][kSides];
-
-  double etsum_barl_miscal_[kNMiscalBinsEB][kBarlRings];
-  double etsum_endc_miscal_[kNMiscalBinsEE][kEndcEtaRings];
+  double etsum_barl_miscal_[kNMiscalBins][kBarlRings];
+  double etsum_endc_miscal_[kNMiscalBins][kEndcEtaRings];
 
   // crystal geometry information
   double cellEta_[kBarlRings];
@@ -111,26 +100,14 @@ class PhiSymmetryCalibration :  public edm::EDAnalyzer
   // factors to convert from ET sum deviation to miscalibration
   double k_barl_[kBarlRings];
   double k_endc_[kEndcEtaRings];
-  double miscalEB_[kNMiscalBinsEB];
-  double miscalEE_[kNMiscalBinsEE]; 
-
+  double miscal_[kNMiscalBins];
+ 
   std::vector<DetId> barrelCells;
   std::vector<DetId> endcapCells;
 
   // input calibration constants
   double oldCalibs_barl[kBarlRings  ][kBarlWedges][kSides];
   double oldCalibs_endc[kEndcWedgesX][kEndcWedgesY][kSides];
-
-  // new calibration constants
-  double newCalibs_barl[kEndcWedgesX][kEndcWedgesX][kSides];
-  double newCalibs_endc[kEndcWedgesX][kEndcWedgesX][kSides];
-
-  // calibration constants not multiplied by old ones
-  float epsilon_M_barl[kBarlRings][kBarlWedges][kSides];
-
-  // Calibration const not corrected for k
-  float rawconst_barl[kBarlRings][kBarlWedges][kSides];
-  float rawconst_endc[kEndcWedgesX][kEndcWedgesX][kSides];   
 
   // steering parameters
 
@@ -146,11 +123,6 @@ class PhiSymmetryCalibration :  public edm::EDAnalyzer
   float phi_endc[kMaxEndciPhi][kEndcEtaRings]; 
   
 
-  bool reiteration_;
-  std::string reiterationXMLFileEB_;
-  std::string reiterationXMLFileEE_;
-  CaloMiscalibMapEcal newCalibs_;
-  EcalIntercalibConstants previousCalibs_;
 };
 
 #endif
